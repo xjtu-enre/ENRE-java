851a16f8c20c6e84d2758a1ffb78d0ff928bc5c9
ba1man <1152575283@qq.com>
2024-01-22 16:32:42 +0800

    update enre-test generation scripts

--- scripts/gen_tests.bat
+++ scripts/gen_tests.bat
@@ -2,9 +2,12 @@
 
 git "submodule" "init"
 git "submodule" "update"
+pushd "enre-java-test" && git "pull" "origin" "main" && git "checkout" "main" && popd
 IF "-d" "%CD%\enre-java-test" (
+  [ "-d" "%CD%\enre-java-test\docs" "]" && DEL /S "%CD%\enre-java-test\docs"
+  [ "-d" "%CD%\enre-java-test\tests" "]" && DEL /S "%CD%\enre-java-test\tests"
   COPY  "%CD%\docs" "enre-java-test\docs"
-  [ NOT "-d" "%CD%\src\test\java\client" "]" && mkdir "-p" "%CD%\src\test\java\client"
-  [ NOT "-d" "%CD%\src\test\resources" "]" && mkdir "-p" "%CD%\src\test\resources"
+  [ NOT "-d" "%CD%\src\test\java\client" "]" && mkdir "-vp" "%CD%\src\test\java\client"
+  [ NOT "-d" "%CD%\src\test\resources" "]" && mkdir "-vp" "%CD%\src\test\resources"
   pushd "enre-java-test" && npm "install" && node "--experimental-specifier-resolution=node" "src\index.js" && popd && COPY  "./enre-java-test/tests/suites/*.java" "./src/test/java/client/" && COPY  "%CD%\enre-java-test\tests\cases" "./src/test/resources/"
 )
--- scripts/gen_tests.sh
+++ scripts/gen_tests.sh
@@ -2,11 +2,16 @@
 
 git submodule init
 git submodule update
+pushd enre-java-test && \
+  git checkout main && \
+  popd
 
 if [ -d "./enre-java-test" ]; then
+  [ -d "./enre-java-test/docs" ] && rm -rfv "./enre-java-test/docs"
+  [ -d "./enre-java-test/tests" ] && rm -rfv "./enre-java-test/tests"
   cp -rv ./docs enre-java-test/docs
-  [ ! -d "./src/test/java/client" ] && mkdir -p "./src/test/java/client"
-  [ ! -d "./src/test/resources" ] && mkdir -p "./src/test/resources"
+  [ ! -d "./src/test/java/client" ] && mkdir -vp "./src/test/java/client"
+  [ ! -d "./src/test/resources" ] && mkdir -vp "./src/test/resources"
   pushd enre-java-test && \
     npm install && \
     node --experimental-specifier-resolution=node "src/index.js" && \
